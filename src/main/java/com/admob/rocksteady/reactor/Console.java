03340fb28b011bc18c368405d2f5c3a4e79e79fb
Jonathan Creasy <jcreasy@box.com>
2012-04-13 03:58:28 -0700

    adding debug output

---
+++ src/main/java/com/admob/rocksteady/reactor/Console.java
@@ -0,0 +1,35 @@
+package com.admob.rocksteady.reactor;
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.UpdateListener;
+
+public class Console implements UpdateListener {
+	private static final Logger logger = LoggerFactory
+			.getLogger(Alerting.class);
+
+	@Override
+	public void update(EventBean[] newEvents, EventBean[] oldEvents) {
+		// TODO Auto-generated method stub
+		if (newEvents == null) {
+			return;
+		}
+
+		Integer i = 0;
+
+		for (EventBean newEvent : newEvents) {
+			i++;
+			try {
+				String msg = "";
+				msg += newEvent.toString();
+				logger.info(msg);
+			} catch (Exception e) {
+				logger.error("Error: " + newEvent.toString() + " - " + e.getMessage());
+			}
+
+		}
+	}
+
+}
